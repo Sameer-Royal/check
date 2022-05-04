937f14532e25c85b64ccdd6552a58f5ff6077f5c
Sameer Royal <SameerRoyal05@gmail.com>
2022-05-04 20:15:29 +0530

    checking the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
+  const { loginWithRedirect,isAuthenticated,logout,user} = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={() => loginWithRedirect()}>Log In</button>;
+        {isAuthenticated&&<h1>USER LOGIN</h1>}
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+    {console.log(user)}
     </div>
   );
 }
